1ed978e7af20d806871b1bd6093517bb9ddc7889
Mixko <apisitmaneerat@gmail.com>
2024-07-30 00:49:51 +0700

    add all skills e2e test

--- e2e/api-test/skill.spec.ts
+++ e2e/api-test/skill.spec.ts
@@ -8,7 +8,7 @@ test.afterAll(deleteData);
 test.afterAll(closeDatabase);
 
 test.describe('GET /skills/e2e_playwright', () => {
-    test('should response todo with status success', async ({request,}) => {
+    test('should response skill with status success', async ({request,}) => {
         const res = await request.get(`/api/v1/skills/e2e_playwright`)
         expect(res.ok()).toBeTruthy()
         expect(await res.json()).toEqual(
@@ -27,6 +27,30 @@ test.describe('GET /skills/e2e_playwright', () => {
                         "testing"
                     ])
                 })
+            }))
+    })
+})
+
+test.describe('GET /skills', () => {
+    test('should response all skills with status success', async ({request,}) => {
+        const res = await request.get(`/api/v1/skills`)
+        expect(res.ok()).toBeTruthy()
+        expect(await res.json()).toEqual(
+            expect.objectContaining({
+                "status": "success",
+                "data": expect.arrayContaining([expect.objectContaining({
+                    "key": "e2e_playwright",
+                    "name": "E2E Playwright",
+                    "description": "Playwright is a Node.js library to automate the Chromium, WebKit, and Firefox browsers with a single API.",
+                    "logo": "https://playwright.dev/img/playwright-logo.svg",
+                    "tags": expect.arrayContaining([
+                        "node",
+                        "javascript",
+                        "typescript",
+                        "automation",
+                        "testing"
+                    ])
+                })])
             }))
     })
 })
